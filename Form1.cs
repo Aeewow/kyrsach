0c8bf1bfdd16bae01fe6123cb50320d4126aa29d
egorn <egor.nazarenko31@mail.ru>
2022-12-12 12:09:56 +0800

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kyrsach
+{
+    public partial class Form1 : Form
+    {
+        List<Particle> particles = new List<Particle>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            for(var i = 0; i < 500; i++)
+            {
+                var particle = new Particle();
+                
+            }
+        }
+
+        private void picDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
