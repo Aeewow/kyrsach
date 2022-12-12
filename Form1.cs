5326f4d6b0184394eab031240fdc538d934b6239
egorn <egor.nazarenko31@mail.ru>
2022-12-12 12:56:33 +0800

    Добавлены частицы и их плавное затухание

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -32,24 +33,44 @@ namespace kyrsach
         {
 
         }
-        int counter = 0;
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1;
+                if(particle.Life < 0)
+                {
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = picDisplay.Image.Width / 2;
+                    particle.Y = picDisplay.Image.Height / 2;
+                    particle.Direction = Particle.rand.Next(360);
+                    particle.Speed = 1 + Particle.rand.Next(10);
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+        }
+
+        private void Render(Graphics g)
+        {
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            counter++;
+            UpdateState();
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
                 g.Clear(Color.White);
-                g.DrawString(
-                    counter.ToString(),
-                    new Font("Arial", 12),
-                    new SolidBrush(Color.Black),
-                    new PointF
-                    {
-                        X = picDisplay.Image.Width / 2,
-                        Y = picDisplay.Image.Height / 2
-                    }
-                );
+                Render(g);
             }
             picDisplay.Invalidate();
         }
--- Particle.cs
+++ Particle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace kyrsach
 {
@@ -15,6 +16,8 @@ namespace kyrsach
         public float Direction;
         public float Speed;
 
+        
+        public float Life;
         public static Random rand = new Random();
 
         public Particle()
@@ -22,6 +25,17 @@ namespace kyrsach
             Direction = rand.Next(360);
             Speed = 1 + rand.Next(10);
             Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100);
+        }
+
+        public void Draw(Graphics g)
+        {
+            float k = Math.Min(1f, Life / 100);
+            int alpha = (int)(k * 255);
+            var color = Color.FromArgb(alpha, Color.Black);
+            var b = new SolidBrush(color);
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+            b.Dispose();
         }
     }
 }
