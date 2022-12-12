8bc32697bec317f2eec8244835ae9f1e28b89edb
egorn <egor.nazarenko31@mail.ru>
2022-12-12 12:31:07 +0800

    Добавили таймер

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,6 +42,12 @@
             this.picDisplay.TabIndex = 0;
             this.picDisplay.TabStop = false;
             this.picDisplay.Click += new System.EventHandler(this.picDisplay_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -57,6 +65,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,16 +19,39 @@ namespace kyrsach
 
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
 
-            for(var i = 0; i < 500; i++)
+            for (var i = 0; i < 500; i++)
             {
                 var particle = new Particle();
-                
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                particles.Add(particle);
             }
         }
 
         private void picDisplay_Click(object sender, EventArgs e)
         {
 
+        }
+        int counter = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);
+                g.DrawString(
+                    counter.ToString(),
+                    new Font("Arial", 12),
+                    new SolidBrush(Color.Black),
+                    new PointF
+                    {
+                        X = picDisplay.Image.Width / 2,
+                        Y = picDisplay.Image.Height / 2
+                    }
+                );
+            }
+            picDisplay.Invalidate();
         }
     }
 }
