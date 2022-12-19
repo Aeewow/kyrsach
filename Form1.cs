3cf4b78dfc5d57c5489ee22dd42ba1f81b97972b
egorn <egor.nazarenko31@mail.ru>
2022-12-19 12:43:34 +0800

    Добавлен вектор скорости

--- Form1.cs
+++ Form1.cs
@@ -13,64 +13,27 @@ namespace kyrsach
 {
     public partial class Form1 : Form
     {
+        Emitter emitter = new Emitter();
         List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
 
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
-
-            for (var i = 0; i < 500; i++)
-            {
-                var particle = new Particle();
-                particle.X = picDisplay.Image.Width / 2;
-                particle.Y = picDisplay.Image.Height / 2;
-                particles.Add(particle);
-            }
         }
 
         private void picDisplay_Click(object sender, EventArgs e)
         {
 
         }
-        private void UpdateState()
-        {
-            foreach (var particle in particles)
-            {
-                particle.Life -= 1;
-                if(particle.Life < 0)
-                {
-                    particle.Life = 20 + Particle.rand.Next(100);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-                    particle.Direction = Particle.rand.Next(360);
-                    particle.Speed = 1 + Particle.rand.Next(10);
-                    particle.Radius = 2 + Particle.rand.Next(10);
-                }
-                else
-                {
-                    var directionInRadians = particle.Direction / 180 * Math.PI;
-                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
-                }
-            }
-        }
-
-        private void Render(Graphics g)
-        {
-            foreach (var particle in particles)
-            {
-                particle.Draw(g);
-            }
-        }
-
+       
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState();
+            emitter.UpdateState();
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                g.Clear(Color.White);
-                Render(g);
+                g.Clear(Color.Black);
+                emitter.Render(g);
             }
             picDisplay.Invalidate();
         }
@@ -78,8 +41,8 @@ namespace kyrsach
         private int MousePositionY = 0;
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            MousePositionX = e.X;
-            MousePositionY = e.Y;
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
